ff4218ce7770b5049c4b90d34edd7c3d29453154
sukhdevmiyatra <141604120+sukhdevmiyatra@users.noreply.github.com>
2023-08-07 10:36:29 +0530

    Update eda_on_smartphone_data.py

--- eda_on_smartphone_data.py
+++ eda_on_smartphone_data.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,112 +6,206 @@ import seaborn as sns
 import streamlit as st
 import plotly.express as px
 
-# Load the dataset
+
 df = pd.read_csv('smartphone_cleaned.csv')
 
-# Set the page configuration for the Streamlit app
+
 st.set_page_config(page_title='Smartphone Data Analysis')
 
-# Create a sidebar menu for selecting different analysis options
-st.sidebar.title('Smartphone Data Analysis Menu')
-menu = st.sidebar.selectbox('Select One', [
-    'Smartphone Data Overview', 
-    'Top 10 Smartphone Brands By Value Count', 
-    'Rating Overview', 
-    'Processor Overview', 
-    'Storage And Ram Overview', 
-    'Operating System Overview', 
-    'Camera Overview', 
-    'Extra Features'
-])
 
-# Handle different menu options
+st.sidebar.title('Smartphone Data Analysis Menu')
+
+menu = st.sidebar.selectbox('Select One',['Smartphone Data Overview','Top 10 Smartphone Brands By Value Count','Rating Overview','Processor Overview','Storage And Ram Overview','Operating System Overview','Camera Overview','Extra Features'])
+
+
 if menu == 'Smartphone Data Overview':
-    # Display a brief overview of the dataset
     st.write('Smartphone Data Overview')
-    st.dataframe(df.head(10))
+    st.dataframe(df.head(10))  
     
-    # Display the statistical description of the dataset
     st.write('Smartphone Data Statistical Description')
-    st.dataframe(df.describe())
     
-    # Perform price to performance analysis using plots and charts
-    st.title('In-Depth Price To Performance Analysis')
+    st.dataframe(df.describe())  
     
-    # Bar chart showing smartphone brands vs. price via rating
-    bar_chart = px.bar(df, x='brand_name', y='price', color='rating')
-    st.plotly_chart(bar_chart)
+    st.title('In Depth Price To Performance Analysis')
     
-    # Bar chart showing processor brand vs. price
-    st.write('Processor Brand vs. Price')
-    fig1, ax1 = plt.subplots()
-    sns.barplot(data=df, x='processor_brand', y='price', estimator=np.median, ax=ax1)
+    st.write('Smartphone Brands Vs Price Via Rating')
+    
+    bar_chart = px.bar(df, x='brand_name', y='price',color='rating')
+    st.plotly_chart(bar_chart) 
+    
+    st.write('Processor Brand vs Price')
+    
+    fig1,ax1 = plt.subplots()
+    sns.barplot(data=df,x='processor_brand',y='price',estimator=np.median,ax=ax1)
     plt.xticks(rotation='vertical')
     st.pyplot(fig1)
     
-    # Bar chart showing processor cores vs. price
-    st.write('Processor Cores vs. Price')
-    fig2, ax2 = plt.subplots()
-    sns.barplot(data=df, x='num_cores', y='price', estimator=np.median, ax=ax2)
+    st.write('Processor Cores vs Price')
+    
+    fig2,ax2 = plt.subplots()
+    sns.barplot(data=df,x='num_cores',y='price',estimator=np.median,ax=ax2)
     st.pyplot(fig2)
     
-    # Scatter plot showing processor speed vs. price
-    st.write('Processor Speed vs. Price')
+    st.write('Processor Speed vs Price')
+    
     fig3, ax3 = plt.subplots()
     sns.scatterplot(data=df, x='processor_speed', y='price', ax=ax3)
-    st.pyplot(fig3)    
+    st.pyplot(fig3)
     
-    # Scatter plot showing processor screen size vs. price
-    st.write('Processor Screen Size vs. Price')
-    fig4, ax4 = plt.subplots()
-    sns.scatterplot(data=df, x='screen_size', y='price', ax=ax4)
+    st.write('Cores And Operating System')
+    
+    st.dataframe(pd.crosstab(df['num_cores'],df['os'])) 
+    
+    st.write('Processor Screen Size vs Price')
+    
+    fig4,ax4 = plt.subplots()      
+    sns.scatterplot(data=df,x='screen_size',y='price',ax=ax4)
     st.pyplot(fig4)
     
-    # Bar chart showing infrared (IR) blaster vs. price
-    st.write('IR Blaster vs. Price')
-    fig5, ax5 = plt.subplots()
-    sns.barplot(data=df, x='has_ir_blaster', y='price', estimator=np.median, ax=ax5)
+    st.write('Ir Blaster vs Price')
+    
+    fig5,ax5 = plt.subplots()    
+    sns.barplot(data=df,x='has_ir_blaster',y='price',estimator=np.median,ax=ax5)
     st.pyplot(fig5)
     
-    # Point plot showing NFC (Near Field Communication) vs. price
-    st.write('NFC vs. Price')
-    fig6, ax6 = plt.subplots()
-    sns.pointplot(data=df, x='has_nfc', y='price', estimator=np.median, ax=ax6)
+    st.write('NFC vs Price')
+    
+    fig6,ax6 = plt.subplots()   
+    sns.pointplot(data=df,x='has_nfc',y='price',estimator=np.median,ax=ax6)
     st.pyplot(fig6)
     
-    # Bar chart showing 5G capability vs. price
-    st.write('5G vs. Price')
-    fig7, ax7 = plt.subplots()
-    sns.barplot(data=df, x='has_5g', y='price', estimator=np.median, ax=ax7)
-    st.pyplot(fig7)
+    st.write('5G vs Price')
+    
+    fig7,ax7 = plt.subplots()  
+    sns.barplot(data=df,x='has_5g',y='price',estimator=np.median,ax=ax7)
+    st.pyplot(fig7)  
+    
+  
 
-# Handle the other menu options with different analysis types
 elif menu == 'Top 10 Smartphone Brands By Value Count':
-    # Display a bar chart of the top 10 smartphone brands based on value counts
+    
     st.write('Top 10 Smartphone Brands By Value Count')
-    bar_chart = df['brand_name'].value_counts().head(10)
-    bar_chart_data = px.bar(bar_chart, text_auto=True)
-    st.plotly_chart(bar_chart_data)
+    
+    bar_chart = df['brand_name'].value_counts().head(10)      
 
+    bar_chart_data = px.bar(bar_chart,text_auto=True)
+
+    st.plotly_chart(bar_chart_data)
+ 
 elif menu == 'Rating Overview':
-    # Display rating-related overviews and analysis
+    
     st.write('Rating Overview')
     
-    # Histogram of smartphone ratings
-    st.pyplot(sns.displot(kind='hist', data=df, x='rating', kde=True))
+    st.pyplot(sns.displot(kind='hist',data=df,x='rating',kde=True))    
     
-    # Bar chart showing rating vs. price
     st.write('Rating Price Analysis')
-    st.plotly_chart(px.bar(df, x='rating', y='price'))
     
-    # Box plot showing rating distribution
+    st.plotly_chart(px.bar(df,x='rating',y='price'))   
+    
     st.write('Rating Boxplot')
-    fig, ax = plt.subplots()
+    
+    fig, ax = plt.subplots()    
     sns.boxplot(data=df, x='rating', ax=ax)
     st.pyplot(fig)
+   
+elif menu == 'Processor Overview':   
     
-    # Cross-tabulation of cores and operating systems
-    st.write('Cores And Operating System')
-    st.dataframe(pd.crosstab(df['num_cores'], df['os']))
+    st.write('Processor Overview')   
+  
+    st.plotly_chart(px.pie(df, names='processor_brand'))
+    
+elif menu == 'Storage And Ram Overview':
+   
+   
+    st.write('Storage And Ram Overview')
+    
+    ram_capacity_counts = df['ram_capacity'].value_counts()
+    labels = ram_capacity_counts.index
+    values = ram_capacity_counts.values
 
-st.write('End of Analysis')
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig.update_layout(title='RAM Capacity Distribution', showlegend=True)
+    st.plotly_chart(fig)
+    
+    internal_memory_counts = df['internal_memory'].value_counts()
+    labels = internal_memory_counts.index
+    values = internal_memory_counts.values
+
+    fig2 = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig2.update_layout(title='Internal Memory Distribution', showlegend=True)
+    st.plotly_chart(fig2)
+    
+elif menu == 'Operating System Overview':
+    
+    st.write('Storage And Ram Overview')
+    
+    os_counts = df['os'].value_counts()
+    labels = os_counts.index
+    values = os_counts.values
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig.update_layout(title='Operating System Distribution', showlegend=True)
+
+    st.plotly_chart(fig)
+    
+elif menu == 'Camera Overview':
+    
+    st.write('Numbers of Cameras')
+    
+    camera_counts = (df['num_rear_cameras'] + df['num_front_cameras']).value_counts()
+    labels = camera_counts.index
+    values = camera_counts.values
+
+    fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig1.update_layout(title='Numbers of Cameras Distribution', showlegend=True)
+
+    st.plotly_chart(fig1)
+    
+    
+    labels = df['primary_camera_rear']
+    values = df['primary_camera_front']
+
+    fig2 = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig2.update_layout(title='Cameras Megapixels', showlegend=True)
+
+    st.plotly_chart(fig2)
+    
+elif menu == 'Extra Features':
+    
+    st.write('Extra Features') 
+    
+    fast_charging =  df['fast_charging_available'].value_counts()
+    labels = fast_charging.index
+    values = fast_charging.values
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig.update_layout(title='Fast Charing Distribution', showlegend=True)
+
+    st.plotly_chart(fig)
+    
+    refresh_rate =  df['refresh_rate'].value_counts()
+    labels = refresh_rate.index
+    values = refresh_rate.values
+
+    fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig1.update_layout(title='Refresh Rate Distribution', showlegend=True)
+
+    st.plotly_chart(fig1)    
+    
+    
+    extended_memory =  df['extended_memory_available'].value_counts() 
+    labels = extended_memory.index
+    values = extended_memory.values
+
+    fig2 = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent+label')])
+
+    fig2.update_layout(title='Extended Memory Distribution', showlegend=True)
+
+    st.plotly_chart(fig2) 
