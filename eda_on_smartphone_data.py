23010733cb6db43f80c35ab8fc1ecdc15c5c8c6b
sukhdevmiyatra <141604120+sukhdevmiyatra@users.noreply.github.com>
2023-08-07 11:22:34 +0530

    Update eda_on_smartphone_data.py

--- eda_on_smartphone_data.py
+++ eda_on_smartphone_data.py
@@ -19,72 +19,89 @@ menu = st.sidebar.selectbox('Select One',['Smartphone Data Overview','Top 10 Sma
 
 
 if menu == 'Smartphone Data Overview':
-    st.write('Smartphone Data Overview')
+    st.title('Smartphone Data Overview')
+   
     st.dataframe(df.head(10))  
-    
-    st.write('Smartphone Data Statistical Description')
+    st.write('It shows the first 10 rows of the smartphone dataset')
+             
+    st.title('Smartphone Data Statistical Description')
     
     st.dataframe(df.describe())  
+
+    st.write('It shows the some basic stats like the average price, highest, lowest, etc')
     
     st.title('In Depth Price To Performance Analysis')
     
-    st.write('Smartphone Brands Vs Price Via Rating')
+    st.write('Smartphone Brands Vs Price Via Rating')    
+    
     
     bar_chart = px.bar(df, x='brand_name', y='price',color='rating')
-    st.plotly_chart(bar_chart) 
+    st.plotly_chart(bar_chart)                     
     
-    st.write('Processor Brand vs Price')
+    st.title('Processor Brand vs Price')
     
     fig1,ax1 = plt.subplots()
     sns.barplot(data=df,x='processor_brand',y='price',estimator=np.median,ax=ax1)
     plt.xticks(rotation='vertical')
     st.pyplot(fig1)
+    st.write("We see a cool bar chart that compares smartphone brands based on their prices and colors them according to their ratings. Looks like higher-rated phones are generally more expensive.")
     
-    st.write('Processor Cores vs Price')
+    st.title('Processor Cores vs Price')
     
     fig2,ax2 = plt.subplots()
     sns.barplot(data=df,x='num_cores',y='price',estimator=np.median,ax=ax2)
     st.pyplot(fig2)
+    st.write("Another bar chart shows the median price of smartphones for different processor brands.")
     
-    st.write('Processor Speed vs Price')
+    st.title('Processor Speed vs Price')
     
     fig3, ax3 = plt.subplots()
     sns.scatterplot(data=df, x='processor_speed', y='price', ax=ax3)
     st.pyplot(fig3)
+    st.write("There are graphs for processor cores and speed vs. price too.")    
     
-    st.write('Cores And Operating System')
-    
-    st.dataframe(pd.crosstab(df['num_cores'],df['os'])) 
-    
-    st.write('Processor Screen Size vs Price')
+    st.title('Processor Screen Size vs Price')
     
     fig4,ax4 = plt.subplots()      
     sns.scatterplot(data=df,x='screen_size',y='price',ax=ax4)
     st.pyplot(fig4)
+    st.write("There is a scatter plot showing how screen size affects the price.")
     
-    st.write('Ir Blaster vs Price')
+    
+    st.title('Ir Blaster vs Price')
     
     fig5,ax5 = plt.subplots()    
     sns.barplot(data=df,x='has_ir_blaster',y='price',estimator=np.median,ax=ax5)
-    st.pyplot(fig5)
+    st.pyplot(fig5)    
     
-    st.write('NFC vs Price')
+    
+    st.title('NFC vs Price')
     
     fig6,ax6 = plt.subplots()   
     sns.pointplot(data=df,x='has_nfc',y='price',estimator=np.median,ax=ax6)
     st.pyplot(fig6)
     
-    st.write('5G vs Price')
+    st.title('5G vs Price')
     
     fig7,ax7 = plt.subplots()  
     sns.barplot(data=df,x='has_5g',y='price',estimator=np.median,ax=ax7)
-    st.pyplot(fig7)  
+    st.pyplot(fig7)
+    st.write("charts show the median price of phones with or without IR blaster, NFC, and 5G, so we can see if these features influence the price.")  
+
+    st.title("Inferences:")
+    st.write("- Higher-rated phones tend to be more expensive.")
+    st.write("- Phones with certain processor brands might have different price ranges.")
+    st.write("- The number of cores and processor speed doesn't seem to have a big impact on the price.")
+    st.write("- Phones with larger screens tend to be pricier.")
+    st.write("- Some features like IR blaster, NFC, and 5G can affect the price.")
     
   
 
 elif menu == 'Top 10 Smartphone Brands By Value Count':
     
-    st.write('Top 10 Smartphone Brands By Value Count')
+    st.title('Top 10 Smartphone Brands By Value Count')
+    
+    st.write('This one gives us the most popular smartphone brands in the dataset.')
     
     bar_chart = df['brand_name'].value_counts().head(10)      
 
@@ -94,15 +111,17 @@ elif menu == 'Top 10 Smartphone Brands By Value Count':
  
 elif menu == 'Rating Overview':
     
-    st.write('Rating Overview')
+    st.title('Rating Overview')
+    st.write("We can see a histogram of smartphone ratings, so we know how they are distributed.")
     
     st.pyplot(sns.displot(kind='hist',data=df,x='rating',kde=True))    
     
-    st.write('Rating Price Analysis')
-    
+    st.title('Rating Price Analysis')
+    st.write("A bar chart shows the relationship between phone ratings and prices. Higher-rated phones tend to be more expensive.")
     st.plotly_chart(px.bar(df,x='rating',y='price'))   
     
-    st.write('Rating Boxplot')
+    st.title('Rating Boxplot')
+    st.write("There's also a box plot to see the spread and outliers in ratings.")
     
     fig, ax = plt.subplots()    
     sns.boxplot(data=df, x='rating', ax=ax)
@@ -110,14 +129,17 @@ elif menu == 'Rating Overview':
    
 elif menu == 'Processor Overview':   
     
-    st.write('Processor Overview')   
+    st.title('Processor Overview')
+    st.write('A simple pie chart shows the distribution of different processor brands used in smartphones.')  
   
     st.plotly_chart(px.pie(df, names='processor_brand'))
     
 elif menu == 'Storage And Ram Overview':
    
    
-    st.write('Storage And Ram Overview')
+    st.title('Storage And Ram Overview')
+
+    st.write('The pie charts tell us the percentage distribution of different RAM capacities and internal memory sizes used in smartphones.')
     
     ram_capacity_counts = df['ram_capacity'].value_counts()
     labels = ram_capacity_counts.index
@@ -139,7 +161,8 @@ elif menu == 'Storage And Ram Overview':
     
 elif menu == 'Operating System Overview':
     
-    st.write('Storage And Ram Overview')
+    st.title('Operating System Overview')
+    st.write('This pie chart shows the distribution of different operating systems used in smartphones.')
     
     os_counts = df['os'].value_counts()
     labels = os_counts.index
@@ -153,7 +176,9 @@ elif menu == 'Operating System Overview':
     
 elif menu == 'Camera Overview':
     
-    st.write('Numbers of Cameras')
+    st.title('Numbers of Cameras')
+    st.write('One pie chart shows the distribution of the total number of rear and front cameras used in smartphones.')
+    st.write('Another pie chart tells us about the distribution of primary camera megapixels.')
     
     camera_counts = (df['num_rear_cameras'] + df['num_front_cameras']).value_counts()
     labels = camera_counts.index
@@ -177,7 +202,8 @@ elif menu == 'Camera Overview':
     
 elif menu == 'Extra Features':
     
-    st.write('Extra Features') 
+    st.title('Extra Features') 
+    st.write('The pie charts show the distribution of features like fast charging, refresh rate, and extended memory availability in smartphones.') 
     
     fast_charging =  df['fast_charging_available'].value_counts()
     labels = fast_charging.index
